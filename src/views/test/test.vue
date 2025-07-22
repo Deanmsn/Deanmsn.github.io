d2d814ec7fe8abd6acd1bf345a537a8e2b9d176f
zeyub <1549859597@qq.com>
2025-07-22 16:23:20 +0800

    test6

--- src/views/test/test.vue
+++ src/views/test/test.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     测试
+    <h3 @click="router.push('/')">
+      主页
+    </h3>
   </div>
 </template>
 
 <script setup lang="ts">
 
+import router from "@/router";
 </script>
 
 <style scoped>
